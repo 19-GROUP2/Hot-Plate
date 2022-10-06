dba468075463023b1915ff2688dcafc803941faa
CSA <abeywickramakcs.19@uom.lk>
2022-10-06 13:34:38 +0530

    serial print with  delay

--- code/sensor.ino
+++ code/sensor.ino
@@ -1,13 +1,22 @@
 #define ZERO 2
 #define SENSOR A0
 #define VR A1
+#define TRIAC 4
 #define SENSOR_MAX 794
 #define SENSOR_MIN 761
 // #define SENSOR_MAX 1023
 // #define SENSOR_MIN 0
 #define ERROR_MAX 50.0
-#define TRIAC 4
-int T = 10;
+
+int zeroPreState = 0;
+int tiracState = 0;
+int newCycle = 0;
+int angle = 5;
+unsigned long count = 0;
+unsigned long zeroNowTime = 0;
+unsigned long zeroPreTime = 0;
+unsigned long triacOnTime = 0;
+unsigned long T = 10;
 
 float getTmpReq()
 {
@@ -21,18 +30,70 @@ float getTmpNow()
 
 int calcAngle()
 {
-    float error = getTmpReq() - getTmpNow();
-    // Serial.print("R ");
-    // Serial.println(getTmpReq());
-    // Serial.print("N ");
-    // Serial.println(getTmpNow());
-    Serial.println(error);
-    int angle = (ERROR_MAX - error) * (float)T / ERROR_MAX;
+    float tmpReq = getTmpReq();
+    float tmpNow = getTmpNow();
+    float error = tmpReq - tmpNow;
+
+    angle = (ERROR_MAX - error) * (float)T / ERROR_MAX;
     if (angle > T)
         angle = T;
     else if (angle < 0)
         angle = 0;
+
+    if (1 || count > 30)
+    {
+        count = 0;
+        Serial.print("R ");
+        Serial.println(tmpReq);
+        Serial.print("N ");
+        Serial.println(tmpNow);
+        Serial.print("E ");
+        Serial.println(error);
+        Serial.print("A ");
+        Serial.println(angle);
+    }
+
     return angle;
+}
+
+void updateTriac()
+{
+    unsigned long nowTime = millis();
+    if (tiracState)
+    {
+        if ((nowTime - triacOnTime) > 0)
+        {
+            tiracState = 0;
+            digitalWrite(TRIAC, tiracState);
+        }
+    }
+    else
+    {
+        if (newCycle && (nowTime - zeroNowTime) > angle)
+        {
+            tiracState = 1;
+            newCycle = 0;
+            digitalWrite(TRIAC, tiracState);
+            triacOnTime = nowTime;
+        }
+    }
+}
+
+void updateTime()
+{
+    int zeroNowState = digitalRead(ZERO);
+    if (zeroNowState != zeroPreState)
+    {
+        tiracState = 0;
+        newCycle = 1;
+        digitalWrite(TRIAC, tiracState);
+        zeroNowTime = millis();
+        T = zeroNowTime - zeroPreTime;
+        // Serial.println(T);
+        zeroPreState = zeroNowState;
+        zeroPreTime = zeroNowTime;
+        count++;
+    }
 }
 
 void setup()
@@ -40,6 +101,8 @@ void setup()
 
     pinMode(VR, INPUT);
     pinMode(SENSOR, INPUT);
+    pinMode(ZERO, INPUT);
+    pinMode(TRIAC, OUTPUT);
 
     Serial.begin(9600);
     Serial.println("start");
@@ -47,7 +110,10 @@ void setup()
 
 void loop()
 {
-    int angle = calcAngle();
-    // Serial.println(angle);
-    delay(200);
+    // int angle = calcAngle();
+    // // Serial.println(angle);
+    // delay(200);
+    updateTime();
+    calcAngle();
+    updateTriac();
 }
